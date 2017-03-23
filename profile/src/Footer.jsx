c4c06c4c3530819734ea45a9975906352731826c
AymanxD <aymanmohatarem@gmail.com>
2017-03-23 14:09:31 -0400

    Working

---
+++ profile/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Icon} from 'react-fa'
+import './CSS/Footer.css';
+
+export default class Footer extends Component{
+  render(){
+    return(
+      <footer>
+        <Icon spin name="spinner" />aymanmohatarem@gmail.com
+      </footer>
+    )
+  }
+}
